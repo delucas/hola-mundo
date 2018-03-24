4b41c699623066129a18da71d1df9e0c6fdec480
Lucas Videla <videla.lucas@gmail.com>
2018-03-24 13:56:26 -0300

    LALA3

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -1,7 +1,13 @@
 package loom.hola_mundo;
 
+/**
+ *	Esto es muy bonito
+ */
 public class HolaMundo {
 
+	/**
+	 *	Esto es más bonito aún
+	 */
     public String saludar(final String persona) {
         return "¡Hola, " + persona + "!";
     }
